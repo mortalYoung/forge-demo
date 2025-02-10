50b20533b3f57fc79e0f558751788f6944c99503
野迂迂 <yangwei1@outlook.com>
2025-02-10 15:48:36 +0800

    Initial commit

---
+++ packages/design-system/index.tsx
@@ -0,0 +1,19 @@
+import { AuthProvider } from '@repo/auth/provider';
+import type { ThemeProviderProps } from 'next-themes';
+import { Toaster } from './components/ui/sonner';
+import { TooltipProvider } from './components/ui/tooltip';
+import { ThemeProvider } from './providers/theme';
+
+type DesignSystemProviderProperties = ThemeProviderProps;
+
+export const DesignSystemProvider = ({
+  children,
+  ...properties
+}: DesignSystemProviderProperties) => (
+  <ThemeProvider {...properties}>
+    <AuthProvider>
+      <TooltipProvider>{children}</TooltipProvider>
+      <Toaster />
+    </AuthProvider>
+  </ThemeProvider>
+);
